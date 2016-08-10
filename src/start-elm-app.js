0f245bc4a1a21187a69315b89f83d13531925423
Peter Szerzo <peter.szerzo@airtame.com>
2016-08-10 09:37:03 +0200

    Notification last dismissed logic in javascript

--- src/start-elm-app.js
+++ src/start-elm-app.js
@@ -1,5 +1,15 @@
 var LOCAL_STORAGE_KEY = 'peterszerzo-com:notification-last-dismissed';
 
+function getTimeSinceNotificationLastDismissed() {
+  var localStorageItem;
+  var lastDismissedAt = window.localStorage
+    ?
+    (window.localStorage.getItem(LOCAL_STORAGE_KEY) ? Number(window.localStorage.getItem(LOCAL_STORAGE_KEY)) : 0)
+    : 0;
+  var now = new Date().getTime();
+  return now - lastDismissedAt;
+}
+
 function setNotificationLastDismissed() {
   if (window.localStorage) {
     window.localStorage.setItem(
@@ -12,9 +22,10 @@ function setNotificationLastDismissed() {
 module.exports = function startApp(window, Elm) {
   var elmApp;
   var node = window.document.getElementById('app');
+  var isNotificationRecentlyDismissed = getTimeSinceNotificationLastDismissed() < 2 * 24 * 3600 * 1000;
   window.requestAnimationFrame(function() {
     node.innerHTML = '';
-    elmApp = Elm.Main.embed(node, true);
+    elmApp = Elm.Main.embed(node, !isNotificationRecentlyDismissed);
     if (window.localStorage) {
       elmApp.ports.notificationDismissed.subscribe(setNotificationLastDismissed);
     }
