f69c2842d8574adaf0ee775bf1f765c5704e6c6f
Peter Szerzo <szerzo.peter@gmail.com>
2016-11-26 12:04:19 +0100

    Fix apostrophes

--- src/App/Content.elm
+++ src/App/Content.elm
@@ -66,7 +66,7 @@ pages =
 
 title : String
 title =
-  "Hi, I am Peter"
+  "Hi, I’m Peter"
 
 subtitle : String
 subtitle =
@@ -83,7 +83,7 @@ I make websites and web apps, spending most of my time on the frontend, sprinkle
 
 I previously studied and dabbled with algorithmic approaches to architectural design, and love to use code outside of the browser from architectural and jewelry design to animation and computational art. And to get a break of them all, I read, write, do yoga and garden. All great things!
 
-You can find me on my bike on the streets of Copenhagen, sitting around in a coffee shop, at the local frontend, React and Elm meetups, [here](https://twitter.com/peterszerzo) and [there](https://medium.com/@peterszerzo) online. Let's talk!
+You can find me on my bike on the streets of Copenhagen, sitting around in a coffee shop, at the local frontend, React and Elm meetups, [here](https://twitter.com/peterszerzo) and [there](https://medium.com/@peterszerzo) online. Let’s talk!
 """
 
 
@@ -94,37 +94,37 @@ aboutReal =
 
 You hit the magic switch. Let me balance out the somewhat conformist professionalism on the other page with some real Peter Szerzo.
 
-I'm a great guy: caring, fun, passionate. That said, when I'm wrapped up in excessive anxiety or self-judgement (which is often), then I can be a bit too much. Ramble ramble ramble.
+I’m a great guy: caring, fun, passionate. That said, when I’m wrapped up in excessive anxiety or self-judgement (which is often), then I can be a bit too much. Ramble ramble ramble.
 
-I judge myself for spending money and I am afraid of gaining weight. I have a fear that one day, due to say world politics, I will have to return to my native Romania, a place I haven't made peace with yet. German society freaks me out.
+I judge myself for spending money and I am afraid of gaining weight. I have a fear that one day, due to say world politics, I will have to return to my native Romania, a place I haven’t made peace with yet. German society freaks me out.
 
-When I was 6, I hit my brother on the back with my fist so hard I felt his entire ribcage resonate through mine. I regret it to this day. Thing is, while my adult brother can forgive me, his child version is just not around. And while we're at my family: for each web project I launch, I write ‘Hi, Mom!’ in the console. Because my mother rocks!
+When I was 6, I hit my brother on the back with my fist so hard I felt his entire ribcage resonate through mine. I regret it to this day. Thing is, while my adult brother can forgive me, his child version is just not around. And while we’re at my family: for each web project I launch, I write ‘Hi, Mom!’ in the console. Because my mother rocks!
 
-I am often in a reflective mood. I often ask myself: ‘What is wrong with this moment?’ Eventually, the answer is always nothing, and the moment of realizing that is genuine happiness to me. The amazing circus of physical sensations in my body are always available to me, and they're incredibly powerful. They are the channel to a sense of belonging. I read all of that in a book, and wonder sometimes if I truly believe these things or just lie to myself about them.
+I am often in a reflective mood. I often ask myself: ‘What is wrong with this moment?’ Eventually, the answer is always nothing, and the moment of realizing that is genuine happiness to me. The amazing circus of physical sensations in my body are always available to me, and they’re incredibly powerful. They are the channel to a sense of belonging. I read all of that in a book, and wonder sometimes if I truly believe these things or just lie to myself about them.
 
-It means a lot to me that you've read this.
+It means a lot to me that you’ve read this.
 """
 
 
 now : String
 now =
     """
-The /now page is Derek Sivers' [brilliant idea](https://sivers.org/nowff). Cheers, Derek!
+The /now page is Derek Sivers’ [brilliant idea](https://sivers.org/nowff). Cheers, Derek!
 
 ## Week of November 7th
 
-Happy to say that [Lettero](https://lettero-27803.firebaseapp.com/) is playable now. There's some usability this and that to flush out, but at least people can try it, and it works ok on a phone.
+Happy to say that [Lettero](https://lettero-27803.firebaseapp.com/) is playable now. There’s some usability this and that to flush out, but at least people can try it, and it works ok on a phone.
 
-Put out two blog posts, [this one](http://blog.peterszerzo.com/im-depressed-and-my-coworkers-love-it/) and [this one](http://blog.peterszerzo.com/high-school-newsletter-interview/), and boy does it feel amazing to write again. What is even more astonishing is that I'm having a much easier time letting go and putting stuff up online.
+Put out two blog posts, [this one](http://blog.peterszerzo.com/im-depressed-and-my-coworkers-love-it/) and [this one](http://blog.peterszerzo.com/high-school-newsletter-interview/), and boy does it feel amazing to write again. What is even more astonishing is that I’m having a much easier time letting go and putting stuff up online.
 
 Anyways, some new ideas for the next week:
 * swimming
-* animating the process of figuring out a matrix' determinant by having a child do hopscotch on the grid of squares
+* animating the process of figuring out a matrix’ determinant by having a child do hopscotch on the grid of squares
 * designing a backgammon set
 
 Excited to getting 1% of all that done :).
 
-Wish all of you, especially the Davis' in Greepoint, a lovely week!
+Wish all of you, especially the Davis’ in Greepoint, a lovely week!
 """
 
 notification : String
