e97f69975fcda121ae0e1a4a0b4459245911e006
peterszerzo <szerzo.peter@gmail.com>
2016-12-30 09:44:46 +0100

    Fix production scripts

--- dev.sh
+++ dev.sh
@@ -1,2 +1,2 @@
 postcss ./src/styles.css --use postcss-cssnext --output ./src/styles.compiled.css --watch &
-elm-app start
+elm-app start &
--- prod.sh
+++ prod.sh
@@ -1,5 +1,5 @@
-postcss ./src/styles.css --use postcss-cssnext --output ./src/styles.compiled.css
-elm-app build
-sed -i -e 's/js\/main/\/js\/main/g' ./dist/index.html
-sed -i -e 's/css\/main/\/css\/main/g' ./dist/index.html
+postcss ./src/styles.css --use postcss-cssnext --output ./src/styles.compiled.css &&
+elm-app build &&
+sed -i -e 's/js\/main/\/js\/main/g' ./dist/index.html &&
+sed -i -e 's/css\/main/\/css\/main/g' ./dist/index.html &&
 cp ./dist/index.html ./dist/200.html
