da578d372b1b8e449f7d96ad44395777850d1b87
peterszerzo <szerzo.peter@gmail.com>
2017-01-05 11:48:27 +0100

    Upgrade create-elm-app. Remove sed scripts

--- prod.sh
+++ prod.sh
@@ -1,5 +1,3 @@
 postcss ./src/styles.css --use postcss-cssnext --output ./src/styles.compiled.css &&
 elm-app build &&
-sed -i -e 's/js\/main/\/js\/main/g' ./dist/index.html &&
-sed -i -e 's/css\/main/\/css\/main/g' ./dist/index.html &&
 cp ./dist/index.html ./dist/200.html
