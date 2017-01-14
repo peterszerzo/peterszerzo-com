78c281263327ef195f68ddc7481ca9a17460ccab
peterszerzo <szerzo.peter@gmail.com>
2017-01-14 09:52:44 +0100

    Little things

--- dev.sh
+++ dev.sh
@@ -1,2 +1,3 @@
+#!/bin/bash
 postcss ./src/styles.css --use postcss-cssnext --output ./src/styles.compiled.css --watch &
 elm-app start &
--- prod.sh
+++ prod.sh
@@ -1,3 +1,4 @@
+#!/bin/bash
 postcss ./src/styles.css --use postcss-cssnext --output ./src/styles.compiled.css &&
 elm-app build &&
 cp ./dist/index.html ./dist/200.html
