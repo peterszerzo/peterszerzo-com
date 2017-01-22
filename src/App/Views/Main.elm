5ba65f403ef4cddda9d014978fa30657db9fde1b
peterszerzo <szerzo.peter@gmail.com>
2017-01-22 08:02:20 +0100

    Simplify views

--- src/App/Views/Main.elm
+++ src/App/Views/Main.elm
@@ -1,18 +1,18 @@
 module Views.Main exposing (..)
 
-import Html exposing (Html, div, map)
-import Html.Attributes exposing (class, src, href)
+import Html exposing (Html, div, text, h1, p, header)
+import Html.Attributes exposing (class, classList, src, href)
+import Html.Events exposing (onClick)
+import Markdown exposing (toHtml)
 import Models exposing (Model, Mode(..))
 import Router
 import Messages exposing (Msg(..))
-import Views.Header
 import Views.TextBox
 import Views.ProjectBox
-import Views.Banner
-import Views.Notification
-import Views.Menu
 import Views.Background
 import Content
+import Views.Shapes exposing (logo, smallLogo, falafel, close)
+import Views.Links exposing (viewMainLinks)
 
 
 view : Model -> Html Msg
@@ -33,7 +33,7 @@ view model =
                     Views.TextBox.view ( Content.aboutConventional, Just Content.aboutReal ) model.mode
 
                 Router.Menu ->
-                    Views.Menu.view model
+                    menu model
 
                 Router.Talks ->
                     Views.TextBox.view ( Content.talks, Nothing ) model.mode
@@ -47,10 +47,101 @@ view model =
             [ div
                 [ class "main__content"
                 ]
-                [ Views.Banner.view
+                [ banner
                 ]
             , content
             , Views.Background.view model
-            , Views.Header.view model
-            , Views.Notification.view model
+            , header_ model
+            , notification model
+            ]
+
+
+banner : Html Msg
+banner =
+    div
+        [ class "banner"
+        ]
+        [ div
+            [ class "banner__logo"
+            ]
+            [ logo
+            , div [ class "banner__image-container" ]
+                [ div [ class "banner__image" ] []
+                , div [ class "banner__overlay" ] []
+                ]
+            ]
+        , h1 [] [ text Content.title ]
+        , p [] [ text Content.subtitle ]
+        ]
+
+
+header_ : Model -> Html Msg
+header_ model =
+    let
+        currentPath =
+            Router.routeToSlug model.route
+
+        isHome =
+            model.route == Router.Home
+    in
+        header
+            [ classList
+                [ ( "header", True )
+                , ( "header--discrete", isHome )
+                ]
+            ]
+            [ div
+                [ class "header__icon header__logo"
+                , onClick
+                    (ChangePath "")
+                ]
+                [ smallLogo ]
+            , viewMainLinks currentPath "header__desktop-links" "header__desktop-link"
+            , div
+                [ class "header__icon header__falafel"
+                , onClick
+                    (ChangePath
+                        (if model.route == Router.Menu then
+                            ""
+                         else
+                            "menu"
+                        )
+                    )
+                ]
+                [ falafel True
+                ]
+            ]
+
+
+notification : Models.Model -> Html.Html Msg
+notification model =
+    div
+        [ classList
+            [ ( "notification", True )
+            , ( "notification--visible", (not model.isNotificationDismissed) && (model.time > 12 && model.time < 75) )
+            ]
+        ]
+        [ toHtml
+            [ class "notification__body"
+            ]
+            Content.notification
+        , div
+            [ class "notification__close"
+            , onClick DismissNotification
+            ]
+            [ close
+            ]
+        ]
+
+
+menu : Model -> Html Msg
+menu model =
+    let
+        currentPath =
+            Router.routeToSlug model.route
+    in
+        div
+            [ class "menu"
+            ]
+            [ viewMainLinks currentPath "menu__links" "menu__link"
             ]
