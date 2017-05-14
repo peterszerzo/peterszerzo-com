01cd2e202e978f77509781a43bb226193f5bfea8
peterszerzo <szerzo.peter@gmail.com>
2017-05-14 13:17:36 +0200

    Add back create-elm-app

--- src/index.js
+++ src/index.js
@@ -1,44 +1,44 @@
-(function() {
-  console.log('Hi, Mom!')
+var Elm = require('./Main.elm')
 
-  var IS_DEV = true
-  var LOCAL_STORAGE_KEY = 'peterszerzo.com:notification-last-dismissed'
+console.log('Hi, Mom!')
 
-  function notificationLastDismissedFor (localStorage) {
-    var now = new Date().getTime()
-    if (!localStorage) {
-      return now
-    }
-    var lastDismissedAt = Number(localStorage.getItem(LOCAL_STORAGE_KEY))
-    if (isNaN(lastDismissedAt)) {
-      return now
-    }
-    return now - lastDismissedAt
+var IS_DEV = process.env.NODE_ENV === 'development'
+var LOCAL_STORAGE_KEY = 'peterszerzo.com:notification-last-dismissed'
+
+function notificationLastDismissedFor (localStorage) {
+  var now = new Date().getTime()
+  if (!localStorage) {
+    return now
   }
-
-  function setNotificationLastDismissed (localStorage) {
-    if (localStorage) {
-      localStorage.setItem(
-        LOCAL_STORAGE_KEY,
-        String(new Date().getTime())
-      )
-    }
+  var lastDismissedAt = Number(localStorage.getItem(LOCAL_STORAGE_KEY))
+  if (isNaN(lastDismissedAt)) {
+    return now
   }
+  return now - lastDismissedAt
+}
 
-  function startApp (node, localStorage) {
-    var elmApp
-    var isNotificationRecentlyDismissed = notificationLastDismissedFor(localStorage) < 2 * 24 * 3600 * 1000
-    window.requestAnimationFrame(function () {
-      node.innerHTML = ''
-      elmApp = Elm.Main.embed(node, {
-        isNotificationRecentlyDismissed: isNotificationRecentlyDismissed,
-        isDev: IS_DEV
-      })
-      if (localStorage) {
-        elmApp.ports.notificationDismissed.subscribe(setNotificationLastDismissed)
-      }
+function setNotificationLastDismissed (localStorage) {
+  if (localStorage) {
+    localStorage.setItem(
+      LOCAL_STORAGE_KEY,
+      String(new Date().getTime())
+    )
+  }
+}
+
+function startApp (node, localStorage) {
+  var elmApp
+  var isNotificationRecentlyDismissed = notificationLastDismissedFor(localStorage) < 2 * 24 * 3600 * 1000
+  window.requestAnimationFrame(function () {
+    node.innerHTML = ''
+    elmApp = Elm.Main.embed(node, {
+      isNotificationRecentlyDismissed: isNotificationRecentlyDismissed,
+      isDev: IS_DEV
     })
-  }
+    if (localStorage) {
+      elmApp.ports.notificationDismissed.subscribe(setNotificationLastDismissed)
+    }
+  })
+}
 
-  startApp(document.getElementById('App'), localStorage)
-}())
+startApp(document.getElementById('App'), localStorage)
