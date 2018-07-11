a26b53607378fa3beb8dcbc9ee1469af9905c225
peterszerzo <szerzo.peter@gmail.com>
2018-07-11 09:05:35 +0200

    Refactor and tweak fragment shader for homepage animation

--- src/Site/Ui/Background.elm
+++ src/Site/Ui/Background.elm
@@ -1,11 +1,11 @@
 module Site.Ui.Background exposing (..)
 
 import Html exposing (Html, Attribute, div)
-import Html.Attributes exposing (class, style, width, height)
 import Math.Vector2 exposing (Vec2, vec2)
 import WebGL
 import Window
 import Time
+import OverEasy.Concepts.SimpleWebGL as SimpleWebGL
 
 
 type alias Vertex =
@@ -25,79 +25,21 @@ type alias Varyings =
     {}
 
 
-type alias Polygon =
-    { coordinates : List ( Float, Float )
-    , opacities : ( Float, Float )
-    }
-
-
-floatRem : Float -> Float -> Float
-floatRem a b =
-    ((a / b) - (a / b |> floor |> toFloat)) * b
-
-
 view : Window.Size -> Time.Time -> Html msg
 view window timeSinceStart =
-    let
-        expand =
-            if (window.width < 800) then
-                100
-            else
-                0
-
-        size =
-            (max window.width window.height) + 2 * expand
-
-        top =
-            (toFloat (size - window.height)) / 2
-
-        left =
-            (toFloat (size - window.width)) / 2
-
-        isTop =
-            window.width > window.height
-
-        scale =
-            (toFloat size) / 100
-    in
-        WebGL.toHtml
-            [ width size
-            , height size
-            , style
-                [ ( "top", "-" ++ (toString top) ++ "px" )
-                , ( "left", "-" ++ (toString left) ++ "px" )
-                , ( "z-index", "1" )
-                , ( "position", "absolute" )
-                ]
-            ]
-            [ WebGL.entity vertexShader
-                fragmentShader
-                mesh
-                { resolution = vec2 (toFloat size) (toFloat size)
-                , time = timeSinceStart / 4000
+    SimpleWebGL.view
+        { window = window
+        , styles = []
+        , makeUniforms =
+            (\resolution ->
+                { resolution = resolution
+                , time = timeSinceStart / 2000
                 , horizontal = window.width > window.height
                 , mobile = window.width < 600
                 }
-            ]
-
-
-mesh : WebGL.Mesh Vertex
-mesh =
-    [ ( vec2 -1.0 -1.0 |> Vertex, vec2 1.0 -1.0 |> Vertex, vec2 -1.0 1.0 |> Vertex )
-    , ( vec2 -1.0 1.0 |> Vertex, vec2 1.0 -1.0 |> Vertex, vec2 1.0 1.0 |> Vertex )
-    ]
-        |> WebGL.triangles
-
-
-vertexShader : WebGL.Shader Vertex Uniforms Varyings
-vertexShader =
-    [glsl|
-attribute vec2 position;
-
-void main() {
-  gl_Position = vec4(position, 0.0, 1.0);
-}
-|]
+            )
+        , fragmentShader = fragmentShader
+        }
 
 
 fragmentShader : WebGL.Shader {} Uniforms Varyings
@@ -109,13 +51,13 @@ uniform vec2 resolution;
 uniform float time;
 
 float rand(float x) {
-  return fract(sin(x) * 43758.5453123);
+  return fract(sin(x) * 4378.5453);
 }
 
 float perlin(float x) {
   float i = floor(x);  // integer
   float f = fract(x);  // fraction
-  return mix(rand(i), rand(i + 1.0), smoothstep(0.,1.,f));
+  return mix(rand(i), rand(i + 1.0), smoothstep(0.0, 1.0, f));
 }
 
 const float n = 26.0;
@@ -124,7 +66,7 @@ float gridFloor(float x) {
   return floor(n * x) / n + 0.5 / n;
 }
 
-const float pi = 3.14159265358979323;
+const float pi = 3.1415926535897;
 const float rotateAngle = 0.3 * pi;
 const mat2 rotate = mat2(cos(rotateAngle), sin(rotateAngle), -sin(rotateAngle), cos(rotateAngle));
 
@@ -158,9 +100,9 @@ void main() {
   if (d < maxDistance) {
     discard;
   } else if (d < maxDistance + 0.08) {
-    gl_FragColor = vec4(0.0, 0.0, 0.0, 0.08 * (d - maxDistance) / 0.08);
+    gl_FragColor = vec4(0.0, 0.0, 0.0, 0.12 * (d - maxDistance) / 0.08);
   } else {
-    gl_FragColor = vec4(0.0, 0.0, 0.0, 0.08);
+    gl_FragColor = vec4(0.0, 0.0, 0.0, 0.12);
   }
 }
 |]
