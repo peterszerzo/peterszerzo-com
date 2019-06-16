edae34975522baff54fa3ef240a27d4884c98a1c
peterszerzo <szerzo.peter@gmail.com>
2019-06-16 06:33:21 +0200

    modifications

--- sketches/elm/Envo.elm
+++ sketches/elm/Envo.elm
@@ -70,45 +70,25 @@ update msg model =
             )
 
 
-floatRange : Int -> List Float
-floatRange n =
-    List.range 0 n |> List.map (\i -> toFloat i / toFloat n)
-
-
-grid2 : Int -> Int -> List ( Float, Float )
-grid2 n m =
-    floatRange n
+grid : Int -> List ( Float, Float )
+grid n =
+    List.range 0 n
         |> List.map
             (\kn ->
-                floatRange m
-                    |> List.map (\km -> ( kn, km ))
+                List.range 0 n
+                    |> List.map (\km -> ( toFloat kn - toFloat n / 2, toFloat km - toFloat n / 2 ))
             )
         |> List.foldl (++) []
 
 
 meshes : List (WebGL.Mesh Vertex)
 meshes =
-    let
-        n : Int
-        n =
-            4
-
-        nf : Float
-        nf =
-            toFloat n
-    in
-    grid2 4 4
+    grid 6
         |> List.map
             (\( kx, ky ) ->
-                ( kx * nf * 1.12 - nf / 2, ky * nf * 1.12 - nf / 2, 0 )
-            )
-        |> List.indexedMap
-            (\index ( x, y, z ) ->
-                quad x y z
+                quad
                     |> List.map
-                        (toVertex
-                            index
-                        )
+                        (toVertex kx ky)
                     |> WebGL.triangleStrip
             )
 
@@ -177,7 +157,7 @@ perspective time =
                 (sin theta * cos phi)
                 (cos theta * cos phi)
                 (sin phi)
-                |> Vector3.scale 4
+                |> Vector3.scale 9.7
                 |> Vector3.add base
     in
     Matrix4.mul (Matrix4.makePerspective 45 1 0.01 100)
@@ -195,7 +175,8 @@ type alias Uniforms =
 type alias Varyings =
     { brightness : Float
     , vUv : Vector2.Vec2
-    , vPanel : Float
+    , vKx : Float
+    , vKy : Float
     }
 
 
@@ -205,21 +186,54 @@ vertexShader =
 attribute vec3 position;
 attribute vec3 normal;
 attribute vec2 uv;
-attribute float panel;
+attribute float kx;
+attribute float ky;
 uniform mat4 perspective;
 uniform mat4 transform;
 uniform float time;
 varying float brightness;
 varying vec2 vUv;
-varying float vPanel;
-const vec3 lightDirection = vec3(0.0, 0.0, 1.0);
+varying float vKx;
+varying float vKy;
+const vec3 lightDirection = vec3(0.0, 0.0, -1.0);
+
+
+mat4 rotationMatrix(vec3 axis, float angle) {
+  axis = normalize(axis);
+  float s = sin(angle);
+  float c = cos(angle);
+  float oc = 1.0 - c;
+   
+  return mat4(
+    oc * axis.x * axis.x + c, oc * axis.x * axis.y - axis.z * s, oc * axis.z * axis.x + axis.y * s, 0.0,
+    oc * axis.x * axis.y + axis.z * s, oc * axis.y * axis.y + c, oc * axis.y * axis.z - axis.x * s, 0.0,
+    oc * axis.z * axis.x - axis.y * s,  oc * axis.y * axis.z + axis.x * s,  oc * axis.z * axis.z + c, 0.0,
+    0.0, 0.0, 0.0, 1.0
+  );
+}
+
+
 void main () {
-  gl_Position = (perspective * transform) * vec4(0.8 * position + vec3(0.0, 0.0, 0.0), 1.0);
-  brightness = 1.0 + (1.0 - dot(
+  mat4 translate = mat4(
+    1.0, 0.0, 0.0, 0.0,
+    0.0, 1.0, 0.0, 0.0,
+    0.0, 0.0, 1.0, 0.0,
+    kx * 1.12, ky * 1.12, 0.0, 1.0
+  );
+  float rotateFact1 = (mod(3.0 * kx + 7.0 * ky, 5.0) - 2.0) / 2.0 * sin(time * 0.001 + (0.3 * kx + 0.8 * ky));
+  float rotateFact2 = (mod(11.0 * kx + 9.0 * ky, 6.0) - 2.5) / 2.5 * sin(time * 0.001 + (0.9 * kx - 0.2 * ky));
+  mat4 rotate1 = rotationMatrix(vec3(1.0, 0.0, 0.0), 0.22 * rotateFact1);
+  mat4 rotate2 = rotationMatrix(vec3(0.0, 1.0, 0.0), 0.25 * rotateFact2);
+
+  mat4 totalTransform = perspective * transform * translate * rotate1 * rotate2;
+
+  gl_Position = totalTransform * vec4(position, 1.0);
+  brightness = 0.7 - 0.3 * dot(
     normalize(vec3(lightDirection)),
-    normalize((transform * vec4(normal, 1.0)).xyz)
-  ));
-  vPanel = panel;
+    normalize((totalTransform * vec4(normal, 1.0)).xyz)
+  );
+  vKx = kx;
+  vKy = ky;
   vUv = uv;
 }
 |]
@@ -233,31 +247,35 @@ precision highp float;
 uniform float time;
 varying vec2 vUv;
 varying float brightness;
-varying float vPanel;
+varying float vKx;
+varying float vKy;
 
 void main() {
   const float units = 3.0;
 
   float workTime = time + 1000.0;
 
-  float sineFactor = sin(workTime / 1000.0 + vPanel * 0.2);
+  float sineFactor = sin(workTime / 1000.0 + (vKx + vKy) * 0.2);
 
-  float angle = vPanel * (1.2) + 0.25 * sineFactor;
+  float angle = (2.0 * vKx + vKy) * (1.2) + 0.25 * sineFactor;
   mat2 rotationMatrix = mat2(cos(angle), -sin(angle), sin(angle), cos(angle));
 
   vec2 workVUv = (vUv - 0.5) * rotationMatrix + 0.5;
 
-  vec3 color1 = vec3(79.0, 76.0, 219.0) / 255.0;
-  vec3 color2 = vec3(23.0, 26.0, 33.0) / 255.0;
+  vec3 blue = vec3(79.0, 76.0, 219.0) / 255.0;
+  vec3 red = vec3(178.0, 26.0, 41.0) / 255.0;
+  vec3 black = vec3(23.0, 26.0, 33.0) / 255.0;
 
-  float threshold = 0.2 + 0.1 * sineFactor;
-  float blur = 1.0 + 0.02 * sin(vUv.x * 101.5);
+  vec3 primaryColor = mix(blue, red, 0.5 + 0.5 * sin(vKx * 13.3 + vKy * 11.9)) * brightness;
+
+  float threshold = 0.3 + 0.1 * sineFactor;
+  float blur = 1.0 + 0.02 * sin(vUv.x * 41.0);
 
   float r = workVUv.x + workVUv.y;
 
   float k = smoothstep(threshold, threshold + blur, workVUv.x + workVUv.y);
 
-  gl_FragColor = vec4(mix(color1, color2, k), 1.0);
+  gl_FragColor = vec4(mix(primaryColor, black, k), 1.0);
 }
 |]
 
@@ -270,7 +288,8 @@ type alias Vertex =
     { position : Vector3.Vec3
     , normal : Vector3.Vec3
     , uv : Vector2.Vec2
-    , panel : Float
+    , kx : Float
+    , ky : Float
     }
 
 
@@ -281,12 +300,13 @@ type alias VertexGeometry =
     }
 
 
-toVertex : Int -> VertexGeometry -> Vertex
-toVertex index geo =
+toVertex : Float -> Float -> VertexGeometry -> Vertex
+toVertex kx ky geo =
     { position = tupleToVec3 geo.position
     , normal = tupleToVec3 geo.normal
     , uv = tupleToVec2 geo.uv
-    , panel = toFloat (3 * index)
+    , kx = kx
+    , ky = ky
     }
 
 
@@ -300,8 +320,8 @@ tupleToVec2 ( x, y ) =
     Vector2.vec2 x y
 
 
-quad : Float -> Float -> Float -> List VertexGeometry
-quad x0 y0 z0 =
+quad : List VertexGeometry
+quad =
     List.range 0 3
         |> List.map
             (\index ->
@@ -332,7 +352,7 @@ quad x0 y0 z0 =
                         else
                             0
                 in
-                { position = ( x + x0, y + y0, z + z0 )
+                { position = ( x, y, z )
                 , normal = ( 0, 0, 1 )
                 , uv = ( u, v )
                 }
