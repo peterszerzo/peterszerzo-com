9b513e00aefec87e609531c2601114c12ac23f9d
peterszerzo <szerzo.peter@gmail.com>
2019-07-03 15:01:51 +0200

    Update envo script

--- sketches/elm/Envo.elm
+++ sketches/elm/Envo.elm
@@ -4,6 +4,7 @@ import Browser
 import Browser.Events as Events
 import Html
 import Html.Attributes
+import Html.Events
 import Json.Decode as Decode
 import Json.Encode as Encode
 import Math.Matrix4 as Matrix4
@@ -25,12 +26,15 @@ main =
 
 
 type alias Model =
-    Setup.Model {}
+    Setup.Model
+        { shader : Int
+        }
 
 
 type Msg
     = Tick Time.Posix
     | Animate Bool
+    | ChangeShader
 
 
 init : Encode.Value -> ( Model, Cmd Msg )
@@ -43,6 +47,7 @@ init flagsValue =
       , startTime = Nothing
       , isAnimating = flags.animating
       , size = flags.size
+      , shader = 0
       }
     , Cmd.none
     )
@@ -69,6 +74,11 @@ update msg model =
             , Cmd.none
             )
 
+        ChangeShader ->
+            ( { model | shader = model.shader + 1 }
+            , Cmd.none
+            )
+
 
 grid : Int -> List ( Float, Float )
 grid n =
@@ -83,7 +93,7 @@ grid n =
 
 meshes : List (WebGL.Mesh Vertex)
 meshes =
-    grid 6
+    grid 10
         |> List.map
             (\( kx, ky ) ->
                 quad
@@ -93,7 +103,7 @@ meshes =
             )
 
 
-view : Model -> Html.Html msg
+view : Model -> Html.Html Msg
 view model =
     let
         time =
@@ -102,17 +112,27 @@ view model =
 
         perspective_ =
             perspective time
+
+        currentFragmentShader =
+            [ fragmentShader
+            , fragmentShader2
+            , fragmentShader3
+            ]
+                |> (\lst -> List.drop (modBy (List.length lst) model.shader) lst)
+                |> List.head
+                |> Maybe.withDefault fragmentShader
     in
     WebGL.toHtml
         [ Html.Attributes.width (floor model.size)
         , Html.Attributes.height (floor model.size)
+        , Html.Events.onClick ChangeShader
         ]
         (meshes
             |> List.map
                 (\mesh ->
                     WebGL.entity
                         vertexShader
-                        fragmentShader
+                        currentFragmentShader
                         mesh
                         { perspective = perspective_
                         , transform = Matrix4.identity
@@ -157,7 +177,7 @@ perspective time =
                 (sin theta * cos phi)
                 (cos theta * cos phi)
                 (sin phi)
-                |> Vector3.scale 9.7
+                |> Vector3.scale 15
                 |> Vector3.add base
     in
     Matrix4.mul (Matrix4.makePerspective 45 1 0.01 100)
@@ -197,7 +217,6 @@ varying float vKx;
 varying float vKy;
 const vec3 lightDirection = vec3(0.0, 0.0, -1.0);
 
-
 mat4 rotationMatrix(vec3 axis, float angle) {
   axis = normalize(axis);
   float s = sin(angle);
@@ -212,18 +231,21 @@ mat4 rotationMatrix(vec3 axis, float angle) {
   );
 }
 
-
 void main () {
+  float gutter = 0.0;
+
   mat4 translate = mat4(
     1.0, 0.0, 0.0, 0.0,
     0.0, 1.0, 0.0, 0.0,
     0.0, 0.0, 1.0, 0.0,
-    kx * 1.12, ky * 1.12, 0.0, 1.0
+    kx * (1.0 + gutter), ky * (1.0 + gutter), 0.0, 1.0
   );
+
   float rotateFact1 = (mod(3.0 * kx + 7.0 * ky, 5.0) - 2.0) / 2.0 * sin(time * 0.001 + (0.3 * kx + 0.8 * ky));
   float rotateFact2 = (mod(11.0 * kx + 9.0 * ky, 6.0) - 2.5) / 2.5 * sin(time * 0.001 + (0.9 * kx - 0.2 * ky));
-  mat4 rotate1 = rotationMatrix(vec3(1.0, 0.0, 0.0), 0.22 * rotateFact1);
-  mat4 rotate2 = rotationMatrix(vec3(0.0, 1.0, 0.0), 0.25 * rotateFact2);
+
+  mat4 rotate1 = rotationMatrix(vec3(1.0, 0.0, 0.0), 0.62 * rotateFact1);
+  mat4 rotate2 = rotationMatrix(vec3(0.0, 1.0, 0.0), 0.66 * rotateFact2);
 
   mat4 totalTransform = perspective * transform * translate * rotate1 * rotate2;
 
@@ -250,6 +272,11 @@ varying float brightness;
 varying float vKx;
 varying float vKy;
 
+const vec3 lightGray = vec3(240.0) / 255.0;
+
+const vec3 color1 = vec3(31.0, 118.0, 150.0) / 255.0;
+const vec3 color2 = vec3(128.0, 222.0, 217.0) / 255.0;
+
 void main() {
   const float units = 3.0;
 
@@ -257,25 +284,100 @@ void main() {
 
   float sineFactor = sin(workTime / 1000.0 + (vKx + vKy) * 0.2);
 
-  float angle = (2.0 * vKx + vKy) * (1.2) + 0.25 * sineFactor;
+  float angle = (2.0 * vKx + vKy) * 1.2 + 0.25 * sineFactor;
+
   mat2 rotationMatrix = mat2(cos(angle), -sin(angle), sin(angle), cos(angle));
 
   vec2 workVUv = (vUv - 0.5) * rotationMatrix + 0.5;
 
-  vec3 blue = vec3(79.0, 76.0, 219.0) / 255.0;
-  vec3 red = vec3(178.0, 26.0, 41.0) / 255.0;
-  vec3 black = vec3(23.0, 26.0, 33.0) / 255.0;
+  vec3 primaryColor = mix(color1, color2, 0.5 + 0.5 * sin(vKx * 5.3 * vKy + vKy * 3.9)) * brightness;
 
-  vec3 primaryColor = mix(blue, red, 0.5 + 0.5 * sin(vKx * 13.3 + vKy * 11.9)) * brightness;
-
-  float threshold = 0.3 + 0.1 * sineFactor;
-  float blur = 1.0 + 0.02 * sin(vUv.x * 41.0);
+  float threshold = -0.1 + 0.1 * sineFactor;
+  float blur = 1.5;
 
   float r = workVUv.x + workVUv.y;
 
   float k = smoothstep(threshold, threshold + blur, workVUv.x + workVUv.y);
 
-  gl_FragColor = vec4(mix(primaryColor, black, k), 1.0);
+  gl_FragColor = vec4(mix(primaryColor, lightGray, k), 1.0);
+}
+|]
+
+
+fragmentShader2 : WebGL.Shader {} Uniforms Varyings
+fragmentShader2 =
+    [glsl|
+precision highp float;
+
+uniform float time;
+varying vec2 vUv;
+varying float brightness;
+varying float vKx;
+varying float vKy;
+
+const vec3 blue = vec3(79.0, 76.0, 219.0) / 255.0;
+const vec3 red = vec3(178.0, 26.0, 41.0) / 255.0;
+const vec3 black = vec3(23.0, 26.0, 33.0) / 255.0;
+const vec3 lightGray = vec3(240.0) / 255.0;
+
+const vec3 lavender = vec3(145.0, 142.0, 244.0) / 255.0;
+const vec3 blue2 = vec3(20.0, 27.0, 65.0) / 255.0;
+
+void main() {
+  const float units = 3.0;
+
+  float workTime = time + 1000.0;
+
+  float sineFactor = sin(workTime / 1000.0 + (vKx + vKy) * 0.2);
+
+  vec2 center = fract(
+    vec2(
+      8.9 * sin(17.0 * vKx + 5.0 * vKy) - workTime * 0.0002,
+      4.8 * sin(13.0 * vKy + 9.4 * vKx) + workTime * 0.00005
+    )
+  ) * 1.8 - vec2(0.4, 0.4);
+
+  float dist = distance(center, vUv);
+
+  float bubbleFactor = 0.5 + 0.5 * sin(vKx * 5.3 + vKy * 11.3);
+
+  float radius = 0.06 + 0.06 * bubbleFactor;
+  float blur = pow(0.5 - bubbleFactor, 2.0) * 0.42;
+  // float blur = 0.05;
+
+  float fadeOutThreshold1 = 0.10;
+  float fadeOutThreshold2 = 0.2;
+
+  vec2 opacityvec = smoothstep(vec2(fadeOutThreshold1), vec2(fadeOutThreshold2), center) - smoothstep(vec2(1.0) - vec2(fadeOutThreshold2), vec2(1.0) - vec2(fadeOutThreshold1), center);
+
+  float opacity = opacityvec.x * opacityvec.y;
+
+  vec3 bubbleColor = mix(lavender, blue2, bubbleFactor);
+
+  gl_FragColor = vec4(mix(lightGray, bubbleColor, (1.0 - smoothstep(radius, radius + blur, dist))), 1.0);
+}
+|]
+
+
+fragmentShader3 : WebGL.Shader {} Uniforms Varyings
+fragmentShader3 =
+    [glsl|
+precision highp float;
+
+uniform float time;
+varying vec2 vUv;
+varying float brightness;
+varying float vKx;
+varying float vKy;
+
+const vec3 blue = vec3(79.0, 76.0, 219.0) / 255.0;
+const vec3 red = vec3(178.0, 26.0, 41.0) / 255.0;
+const vec3 black = vec3(23.0, 26.0, 33.0) / 255.0;
+const vec3 lightGray = vec3(240.0) / 255.0;
+
+void main() {
+  float factor = smoothstep(0.0, 1.0, 0.2 * (0.4 * vKx - 0.4 * vKy + 2.0));
+  gl_FragColor = vec4(mix(black, lightGray, factor), 1.0);
 }
 |]
 
